9c35c344c8aed01ac3a683eb15074e168286cf28
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-22 00:07:53 +0200

    Starter For Spring JDBC in memory authenticaion with security

---
+++ JDBCAuthentication/src/main/java/com/gk/JDBCSpringSecurity/JDBCAuthentication/JdbcAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.gk.JDBCSpringSecurity.JDBCAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcAuthenticationApplication.class, args);
+	}
+
+}

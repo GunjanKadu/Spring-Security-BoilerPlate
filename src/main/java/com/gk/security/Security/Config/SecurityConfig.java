977ce939ae469893655906e8b0bfe0ebd6a4f53b
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-21 22:45:15 +0200

    Adding different authorization to users and admin

--- src/main/java/com/gk/security/Security/Config/SecurityConfig.java
+++ src/main/java/com/gk/security/Security/Config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.gk.security.Security.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -11,6 +12,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 //Because we are dealing with request and response we need to use enable web security we can also use #MethodSecurity
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // Password encode to store password in hashed format as required by security.
+    // But now we are just storing normal password for learning purpose
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -26,11 +35,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("ADMIN");
     }
 
-    // Password encode to store password in hashed format as required by security.
-    // But now we are just storing normal password for learning purpose
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    // Configuration for roles i.e which user is able to access which API
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // Most Restrictive To Least Restrictive in terms of order
+                .antMatchers( "static/css", "static/js").permitAll() //Static files permit for all users
+                .antMatchers("/admin").hasRole("ADMIN") // .hasAnyRole("USER","ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
-
 }

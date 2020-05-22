a946fc1e3428ee09a32c395d6dadf87b6d3afa17
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-23 00:53:38 +0200

    Returning My User Details in MyUserDetailsService

--- SpringSecurityJPA/src/main/java/com/gk/SpringSecurityJPA/SpringSecurityJPA/Entity/MyUserDetailsService.java
+++ SpringSecurityJPA/src/main/java/com/gk/SpringSecurityJPA/SpringSecurityJPA/Entity/MyUserDetailsService.java
@@ -1,16 +1,28 @@
 package com.gk.SpringSecurityJPA.SpringSecurityJPA.Entity;
 
+import com.gk.SpringSecurityJPA.SpringSecurityJPA.DAO.UserRepository;
 import com.gk.SpringSecurityJPA.SpringSecurityJPA.Service.MyUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new MyUserDetails(s);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Now we fetch the users from the db using the spring jpa repository
+        Optional<User> user = userRepository.findByUserName(username);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found" + username));
+
+        return user.map(MyUserDetails::new).get();
     }
 }

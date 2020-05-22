fd5d7364abd9aa54413bbb152349f6b5f13e71eb
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-05-22 11:01:22 +0200

    Defining an data source for telling how our database is structured and adding default users to the h2 schema provided by spring

--- JDBCAuthentication/src/main/java/com/gk/JDBCSpringSecurity/JDBCAuthentication/Config/SecurityConfig.java
+++ JDBCAuthentication/src/main/java/com/gk/JDBCSpringSecurity/JDBCAuthentication/Config/SecurityConfig.java
@@ -1,14 +1,37 @@
 package com.gk.JDBCSpringSecurity.JDBCAuthentication.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // Defining an data source for telling how our database is structured.
+    // We need to have a Configuration somewhere else and then inject it here.
+    // Now because we are using h2 database we don't need to configure the db because it is already embedded in spring.
+    @Autowired
+    DataSource dataSource;
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication();
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema() // creating the default db schema
+                .withUser(
+                        User.withUsername("user")
+                        .password("pass")
+                        .roles("USER")
+                )
+                .withUser(
+                        User.withUsername("admin")
+                                .password("pass")
+                                .roles("ADMIN")
+                );
     }
 
     @Override
